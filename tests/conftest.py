ebd7ac77918eeacae56e76a2401cf9a1be72c4d9
KseniiaTr <ks31993@mail.ru>
2023-01-17 22:58:26 -0800

    First commit

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+import pytest
+from selenium import webdriver
+from selene import Browser, Config
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser',
+        help='Браузер, в котором будут запущены тесты',
+        choices=['firefox', 'chrome'],
+        default='chrome'
+    )
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_name = request.config.getoption('--browser')
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": browser_name,
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser = Browser(Config(driver))
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
+
